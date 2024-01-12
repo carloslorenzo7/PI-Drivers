1547a768fb557aa17cd4ec3bbc86d8f233b21ecd
carloslorenzo7 <carlos__lorenzo@hotmail.com>
2024-01-11 23:19:44 -0300

    arreglando post en form

--- cr-pi-drivers-main/server/src/server.js
+++ cr-pi-drivers-main/server/src/server.js
@@ -8,10 +8,11 @@ const server = express();
 server.use(morgan("dev"));
 server.use(express.json());
 // version original 
-// server.use(cors());
+server.use(cors());
 
 // version para que ande el form 
-server.use(cors({ origin: 'https://pi-drivers-gamma.vercel.app/' }));
+// server.use(cors({ origin: 'https://pi-drivers-gamma.vercel.app/' }));
+
 server.use(router);
 
 module.exports = server;
